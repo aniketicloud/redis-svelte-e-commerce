3e1370a2189291c282de5b60761f369ebaceba03
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-12-24 18:38:07 +0530

    create user implementation

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,3 +1,5 @@
+import { usersKey } from '$services/keys';
+import { client } from '$services/redis';
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 
@@ -5,4 +7,11 @@ export const getUserByUsername = async (username: string) => {};
 
 export const getUserById = async (id: string) => {};
 
-export const createUser = async (attrs: CreateUserAttrs) => {};
+export const createUser = async ({ password, username }: CreateUserAttrs) => {
+	const id = genId();
+	await client.hSet(usersKey(id), {
+		username,
+		password
+	});
+	return id;
+};
